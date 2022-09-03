5eb6442dec6ae2cdb4aac56a53ae46d07c464de6
Manishsurti <109143099+Manishsurti13@users.noreply.github.com>
2022-09-03 11:43:18 +0100

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Manish on 03/09/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
